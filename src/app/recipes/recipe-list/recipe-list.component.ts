4f80247455e6651805a879560b853cee139e10fb
Tan Loc <tanlocit20062000@gmail.com>
2022-03-26 14:10:18 +0700

    lab02

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from "../recipe.model";
 
 @Component({
@@ -13,9 +13,15 @@ export class RecipeListComponent implements OnInit {
     new Recipe("A Test Recipe", "This is simply a test", "https://scontent.fsgn5-14.fna.fbcdn.net/v/t1.6435-1/116241171_126364569134121_8182763672981624046_n.png?stp=dst-png_p148x148&_nc_cat=101&ccb=1-5&_nc_sid=1eb0c7&_nc_ohc=tGarjpYkdZ4AX_Lewmd&_nc_ht=scontent.fsgn5-14.fna&oh=00_AT-OzZnrBfiAyN2YcINB_GAhE_cKNF10wlOfOiZk-PX_hQ&oe=625B3407")
   ];
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from './recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class RecipesComponent implements OnInit {
 
   constructor() { }
+  selectedRecipe!: Recipe;
 
   ngOnInit(): void {
   }
