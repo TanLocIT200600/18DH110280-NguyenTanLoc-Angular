66c914f4fcec7c75307f3c257dd1432c84fa594e
Khoa Vu <khoa.vu@kyanon.digital>
2022-05-07 14:23:49 +0700

    angular 7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ShoppingListComponent } from '././shopping-list/shopping-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipesComponent } from './recipes/recipes.component';
 
@@ -9,7 +10,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/recipe', pathMatch: 'full' },
   {
     path: 'recipes', component: RecipesComponent, children: [
-      { path: '', component: RecipeStartComponent },
+      { path: '', component: RecipeEditComponent },
       { path: ':id', component: RecipeDetailComponent }
     ]
   },
